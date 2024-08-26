e5dbf5e405a6f776537d01f1651bcb3cc48a1728
Mansoor Sajjad <mansoor.sajjad@entur.org>
2024-08-26 12:15:39 +0200

    Initial commit

---
+++ src/main/java/no/entur/nuska/NuskaApplication.java
@@ -0,0 +1,13 @@
+package no.entur.nuska;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuskaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuskaApplication.class, args);
+	}
+
+}
